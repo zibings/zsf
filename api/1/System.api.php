ee80559fc8b93223c22d71d017c4db8f7b906096
Andrew Male <mamale@outlook.com>
2022-09-23 02:51:16 -0400

    Adding System/DashboardMetrics endpoint

--- api/1/System.api.php
+++ api/1/System.api.php
@@ -2,11 +2,15 @@
 
 	namespace Api1;
 
+	use Stoic\Log\Logger;
 	use Stoic\Web\Api\Response;
+	use Stoic\Web\Api\Stoic;
 	use Stoic\Web\Request;
 
 	use Zibings\ApiController;
+	use Zibings\RoleStrings;
 	use Zibings\SettingsStrings;
+	use Zibings\Users;
 
 	/**
 	 * API controller that provides basic system endpoints.
@@ -15,14 +19,55 @@
 	 */
 	class System extends ApiController {
 		/**
+		 * @param Stoic $stoic Internal instance of Stoic API object.
+		 * @param \PDO $db Internal instance of PDO object.
+		 * @param Logger|null $log Optional Logger object for internal use.
+		 * @param Users|null $users Optional Users repository object for internal use.
+		 * @return void
+		 */
+		public function __construct(
+			Stoic $stoic,
+			\PDO $db,
+			Logger $log = null,
+			protected ?Users $users = null) {
+			parent::__construct($stoic, $db, $log);
+
+			if ($this->users === null) {
+				$this->users = new Users($this->db, $this->log);
+			}
+
+			return;
+		}
+
+		/**
 		 * Registers the controller endpoints.
 		 *
 		 * @return void
 		 */
 		protected function registerEndpoints() : void {
-			$this->registerEndpoint('GET',  '/^System\/Version\/?/i',      'getVersion');
+			$this->registerEndpoint('GET', '/^System\/Version\/?/i',          'getVersion',          false);
+			$this->registerEndpoint('GET', '/^System\/DashboardMetrics\/?/i', 'getDashboardMetrics', RoleStrings::ADMINISTRATOR);
 
 			return;
+		}
+
+		/**
+		 * Retrieves basic dashboard statistics for administrators.
+		 *
+		 * @param Request $request The current request which routed to the endpoint.
+		 * @param array|null $matches Array of matches returned by endpoint regex pattern.
+		 * @return Response
+		 */
+		public function getDashboardMetrics(Request $request, array $matches = null) : Response {
+			$ret = $this->newResponse();
+			$ret->setData([
+				'dau'  => $this->users->getDailyActiveUserCount(),
+				'mau'  => $this->users->getMonthlyActiveUserCount(),
+				'tu'   => $this->users->getTotalUsers(),
+				'tvu'  => $this->users->getTotalVerifiedUsers()
+			]);
+
+			return $ret;
 		}
 
 		/**
