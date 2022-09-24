95932500f1af21e37b1f140d9c2ec4ab23250c69
Andrew Male <mamale@outlook.com>
2022-09-24 01:05:12 -0400

    Adding API location output to start-dev script

--- Start-Dev.ps1
+++ Start-Dev.ps1
@@ -1,4 +1,5 @@
 Start-Job -Name ZsfNpmDev -ScriptBlock{cd admin && yarn install && npm run dev}
 Start-Job -Name ZsfPhpDev -ScriptBlock{cd web && php -S localhost:8080}
 
-Write-Host "`nNow running on http://127.0.0.1:5173/"
+Write-Host "`nAdmin running on http://127.0.0.1:5173/"
+Write-Host "API running on   http://127.0.0.1:8080/api/1/index.php?url="
