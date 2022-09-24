63bf9ad28071919b9b79f56b7b0e16b9a68adf19
Andrew Male <mamale@outlook.com>
2022-09-24 01:03:07 -0400

    Accidentally removed API from dev runs

--- Start-Dev.ps1
+++ Start-Dev.ps1
@@ -1,3 +1,4 @@
 Start-Job -Name ZsfNpmDev -ScriptBlock{cd admin && yarn install && npm run dev}
+Start-Job -Name ZsfPhpDev -ScriptBlock{cd web && php -S localhost:8080}
 
 Write-Host "`nNow running on http://127.0.0.1:5173/"
