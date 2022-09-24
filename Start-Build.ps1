369321147e39a4666778407f73476f0f30f1906e
Andrew Male <mamale@outlook.com>
2022-09-24 01:40:28 -0400

    Modifying start scripts to be nicey

--- Start-Build.ps1
+++ Start-Build.ps1
@@ -1,4 +1,30 @@
+Write-Host "Cleaning output files.. " -NoNewLine
+
+$toDelete = @{
+  "assets"      = "dir"
+  "data"        = "dir"
+  "images"      = "dir"
+  "layout"      = "dir"
+  "themes"      = "dir"
+  "config.json" = "file"
+  "favicon.ico" = "file"
+  "index.html"  = "file"
+}
+
+$toDelete.Keys | ForEach-Object {
+  if (Test-Path "./web/$_") {
+    if ($toDelete[$_] -eq "dir") {
+      Remove-Item "./web/$_" -Recurse
+    } else {
+      Remove-Item "./web/$_"
+    }
+  }
+}
+
+Write-Host "DONE"
+
 cd admin
-yarn install
-npm run build
+yarn
+yarn upgrade
+yarn zsf
 cd ..
--- Start-Dev.ps1
+++ Start-Dev.ps1
@@ -1,4 +1,4 @@
-Start-Job -Name ZsfNpmDev -ScriptBlock{cd admin && yarn install && npm run dev}
+Start-Job -Name ZsfNpmDev -ScriptBlock{cd admin && yarn && yarn upgrade && yarn dev}
 Start-Job -Name ZsfPhpDev -ScriptBlock{cd web && php -S localhost:8080}
 
 Write-Host "`nAdmin running on http://127.0.0.1:5173/"
