78e5ddfc26cfe8e49790dced733788ba4ddbc946
Andrew Male <mamale@outlook.com>
2023-01-19 02:45:01 -0500

    Updating to single PS1 for container; Docs in README

---
+++ Exec.ps1
@@ -0,0 +1,111 @@
+[CmdletBinding()]
+
+$ProjectName      = ""
+$Commands         = @()
+$EnvVariables     = @{}
+$IsInteractive    = $false
+$FoundProjectName = $false
+$HasEnvFile       = Test-Path -Path "./docker/.env"
+
+$args | ForEach-Object {
+	if ($_ -eq "projectname") {
+		$FoundProjectName = $true
+
+		return
+	}
+
+	if ($_ -eq "-i") {
+		$IsInteractive = $true
+
+		return
+	}
+
+	if ($FoundProjectName) {
+		$ProjectName      = $_
+		$FoundProjectName = $false
+
+		return
+	}
+
+	$Commands += $_
+}
+
+if ($HasEnvFile) {
+  Get-Content ./docker/.env | ForEach-Object {
+    $name, $value = $_.split('=')
+
+    $EnvVariables.Add($name, $value)
+  }
+}
+
+if ([string]::IsNullOrWhiteSpace($ProjectName)) {
+  if (!$HasEnvFile -or !$EnvVariables.ContainsKey('PROJECT_NAME')) {
+    $ProjectName = Read-Host -Prompt "What is the name of your project"
+  } else {
+    $ProjectName = $EnvVariables.PROJECT_NAME
+  }
+}
+
+$Command      = $Commands -Join " "
+$WebContainer = "$ProjectName-web"
+
+function InitDocker([string] $ProjectName, [string] $WebContainer) {
+	$envContents  = @"
+PROJECT_NAME=$($ProjectName)
+"@
+
+	Push-Location ./docker
+	$envContents | Out-File -FilePath .env
+	docker build .
+	docker compose -p $ProjectName up -d
+	Pop-Location
+
+	Write-Host "Docker container build complete"
+
+	Write-Host "Waiting 15s to let docker do its thang.. " -NoNewline
+	Start-Sleep -Seconds 15
+	Write-Host "DONE"
+
+	Write-Host "Initializing container.."
+
+	docker exec -t $WebContainer cp docker/siteSettings.json siteSettings.json
+	docker exec -t $WebContainer vendor/bin/stoic-migrate up
+
+	Write-Host "Docker container initialized"
+}
+
+function UpdateDocker([string] $ProjectName, [string] $WebContainer) {
+	Write-Host "Updating container for '$ProjectName'.."
+
+	docker exec -t $WebContainer vendor/bin/stoic-migrate up
+
+	Write-Host "Finished updating"
+}
+
+function StopDocker {
+	Write-Host "Stopping docker container.. "
+
+	Push-Location docker
+	docker compose stop
+	Pop-Location
+
+	Write-Host "Docker container stopped"
+}
+
+Write-Host "Executing command on '$ProjectName' project: $Command"
+
+if ($Command -eq "init") {
+	InitDocker -ProjectName $ProjectName -WebContainer $WebContainer
+} elseif ($Command -eq "update") {
+	UpdateDocker -ProjectName $ProjectName -WebContainer $WebContainer
+} elseif ($Command -eq "stop") {
+	StopDocker
+} else {
+	$opts = "t"
+
+	if ($IsInteractive) {
+		$opts += "i"
+	}
+
+	Invoke-Expression "docker exec -$opts $WebContainer $Command"
+}
--- README.md
+++ README.md
@@ -19,3 +19,48 @@ php scripts/generate-openapi.php -h
 ```
 
 Once generated, you can place this inside of `web` so that it can be utilized by things such as AXIOS.
+
+## Docker Container Notes
+Dependencies required for local development:
+
+* Docker
+* NodeJS (v >= 16)
+
+To initialize the container and run the services:
+
+```powershell
+./Exec.ps1 init
+```
+
+This will make the `~/web` folder accessible via [http://localhost:8080/](http://localhost:8080/) and the MySQL 'adminer' interface available at [http://localhost:8081/](http://localhost:8081/).  To use the 'adminer' interface, use the following connection info:
+
+```
+host: db
+user: root
+pass: P@55word
+name: zsf
+```
+
+To run the migration command and any other common 'update' procedures:
+
+```powershell
+./Exec.ps1 update
+```
+
+To run custom commands on the container:
+
+```powershell
+./Exec.ps1 php scripts/generate-openapi.php -v 1.1 -o ./web/sui/openapi.yaml -f yaml
+```
+
+To run an interactive script on the container:
+
+```powershell
+./Exec.ps1 -i php scripts/add-user.php
+```
+
+To build the front-end and deploy it to the `~/web` folder:
+
+```powershell
+./Exec.ps1 build
+```
