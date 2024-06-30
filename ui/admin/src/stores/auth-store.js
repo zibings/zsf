2fd6a88451d1c53fa61419503933907ec3585862
Andrew Male <mamale@outlook.com>
2024-06-30 11:12:13 -0700

    Making getLoggedIn a little more resilient

--- ui/admin/src/stores/auth-store.js
+++ ui/admin/src/stores/auth-store.js
@@ -7,17 +7,23 @@ export const useAuthStore = defineStore("auth", () => {
 
 	async function getLoggedIn() {
 		const token = localStorage.getItem("authToken");
-		useApi().post("/1.1/Account/CheckToken", { token }).then(res => {
-			if (res.status === 200) {
-				loggedIn.value = true;
+
+		try {
+			useApi().post("/1.1/Account/CheckToken", { token }).then(res => {
+				if (res.status === 200) {
+					loggedIn.value = true;
+
+					return;
+				}
+
+				localStorage.removeItem("authToken");
 
 				return;
-			}
-
+			});
+		} catch (error) {
+			loggedIn.value = false;
 			localStorage.removeItem("authToken");
-
-			return;
-		});
+		}
 
 		return;
 	}
