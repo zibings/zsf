3922d7c18fd84eb8f699fd146861b0b6126120f1
Andrew Male <mamale@outlook.com>
2024-07-03 18:12:53 -0700

    Adding basic top navbar

---
+++ ui/front/src/layout/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="app-wrapper">
+		<NavBar />
+
+		<main>
+			<h1>Howdy, Partner</h1>
+		</main>
+	</div>
+</template>
+
+<script setup lang="ts">
+import NavBar from 'layout/MainLayout/NavBar.vue';
+</script>
+
+<style scoped>
+
+</style>
--- ui/front/src/router/routes.js
+++ ui/front/src/router/routes.js
@@ -1,24 +1,30 @@
 const routes = [
 	{
 		path: "/",
-		name: "home",
-		component: () => import("views/HomeView.vue"),
-	},
-	{
-		path: "/login",
-		name: "login",
-		component: () => import("views/Login.vue"),
-	},
-	{
-		path: "/register",
-		name: "register",
-		component: () => import("views/Register.vue"),
-	},
-	{
-		path: "/profile",
-		name: "profile",
-		meta: { requiresAuth: true },
-		component: () => import("views/Profile.vue"),
+		component: () => import("layout/MainLayout.vue"),
+		children: [
+			{
+				path: "/",
+				name: "home",
+				component: () => import("views/HomeView.vue"),
+			},
+			{
+				path: "/login",
+				name: "login",
+				component: () => import("views/Login.vue"),
+			},
+			{
+				path: "/register",
+				name: "register",
+				component: () => import("views/Register.vue"),
+			},
+			{
+				path: "/profile",
+				name: "profile",
+				meta: { requiresAuth: true },
+				component: () => import("views/Profile.vue"),
+			},
+		]
 	},
 ];
 
