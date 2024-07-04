446b9ec6244f9cc092d5091ead4e46278fc4b83e
Andrew Male <mamale@outlook.com>
2024-07-04 08:16:48 -0700

    Adding role store to front

--- ui/front/src/stores/roles.ts
+++ ui/front/src/stores/roles.ts
@@ -3,8 +3,34 @@ import { defineStore } from 'pinia';
 import { useApi } from 'composables/useApi';
 import { useGeneralStore } from 'stores/general';
 
+interface Role {
+	created: string;
+	id: number;
+	name: string;
+}
+
 export const useRoleStore = defineStore("auth", () => {
 	const api = useApi();
+	const roles = ref<Record<string, Role>>({ });
+	const generalStore = useGeneralStore();
 
-	async function getUserRoles():
+	async function getUserRoles(): Promise<Record<string, Role>> {
+		try {
+			const roleRes = await api.get<Record<string, Role>>("/1.1/Roles/UserRoles");
+
+			if (roleRes.status === 200) {
+				roles.value = roleRes.data;
+			}
+		} catch (error) {
+			if (generalStore.environment === 'development') {
+				console.error(error);
+			}
+
+			roles.value = {};
+		}
+
+		return roles.value;
+	}
+
+	return { getUserRoles };
 });
