41ceafc8309087a7ac7cf9200e93f74069b2ff36
Andrew Male <mamale@outlook.com>
2024-07-15 12:35:17 -0400

    Adding patched Editor primevue component

---
+++ ui/admin/src/components/custom/PatchedEditor.vue
@@ -0,0 +1,34 @@
+<template>
+	<Editor @load="onLoad" @update:model-value="onChange" />
+</template>
+
+<script setup>
+import Editor from "primevue/editor";
+
+const props = defineProps({
+	modelValue: {
+		type: String,
+		default: "",
+	}
+});
+
+const emits = defineEmits(["update:modelValue"]);
+
+const onLoad = ({instance}) => {
+	instance.setContents(instance.clipboard.convert({
+		html: props.modelValue
+	}));
+
+	return;
+};
+
+const onChange = (v) => {
+	emits("update:modelValue", v);
+
+	return;
+};
+</script>
+
+<style scoped>
+
+</style>
