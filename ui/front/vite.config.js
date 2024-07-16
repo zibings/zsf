d0fa5936dba59c8942bf48e2c28af2dc847cde53
Andrew Male <mamale@outlook.com>
2024-07-15 20:01:27 -0400

    Removing vue-dev-tools, that thing is a memory HOG

--- ui/front/vite.config.js
+++ ui/front/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import VueDevTools from "vite-plugin-vue-devtools";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), VueDevTools(), tsconfigPaths()],
+	plugins: [vue(), tsconfigPaths()],
 });
