4e0aa01334eb4dc05b99c7fee8a4d6ab5a979ace
Andrew Male <mamale@outlook.com>
2024-11-19 23:52:50 -0500

    UserAuthHistory class test & some repo bugfixes

--- inc/repositories/UserAuthHistories.rpo.php
+++ inc/repositories/UserAuthHistories.rpo.php
@@ -3,6 +3,7 @@
 	namespace Zibings;
 
 	use Stoic\Pdo\BaseDbQueryTypes;
+	use Stoic\Pdo\PdoDrivers;
 	use Stoic\Pdo\StoicDbClass;
 	
 	/**
@@ -31,6 +32,24 @@
 		}
 
 		/**
+		 * Removes any auth histories present for the given user.
+		 *
+		 * @param int $userId Integer identifier for the user in question.
+		 * @return void
+		 */
+		public function deleteAllForUser(int $userId) : void {
+			$this->tryPdoExcept(function () use ($userId) {
+				$stmt = $this->db->prepare($this->uahObj->generateClassQuery(BaseDbQueryTypes::DELETE, false) . " WHERE UserID = :userId");
+				$stmt->bindValue(':userId', $userId, \PDO::PARAM_INT);
+				$stmt->execute();
+
+				return;
+			}, "Failed to delete user auth histories");
+
+			return;
+		}
+
+		/**
 		 * Retrieves all or a subset of a user's authentication history.
 		 *
 		 * @param int $userId Integer identifier for the user in question.
@@ -40,10 +59,20 @@
 		 */
 		public function getUserAuthHistory(int $userId, ?int $offset = null, ?int $limit = null) : array {
 			$ret = [];
-			$sql = $this->uahObj->generateClassQuery(BaseDbQueryTypes::SELECT, false) . " WHERE [UserID] = :userId ORDER BY [Recorded] DESC";
+			$sql = $this->uahObj->generateClassQuery(BaseDbQueryTypes::SELECT, false);
 
-			if ($offset !== null && $offset >= 0 && $limit === null && $limit > 0) {
-				$sql .= " OFFSET {$offset} ROWS FETCH NEXT {$limit} ROWS ONLY";
+			if ($this->db->getDriver()->is(PdoDrivers::PDO_SQLSRV)) {
+				$sql .= " WHERE [UserID] = :userId ORDER BY [Recorded] DESC";
+
+				if ($offset !== null && $offset >= 0 && $limit === null && $limit > 0) {
+					$sql .= " OFFSET {$offset} ROWS FETCH NEXT {$limit} ROWS ONLY";
+				}
+			} else if ($this->db->getDriver()->is(PdoDrivers::PDO_MYSQL)) {
+				$sql .= " WHERE `UserID` = :userId ORDER BY `Recorded` DESC";
+
+				if ($offset !== null && $offset >= 0 && $limit === null && $limit > 0) {
+					$sql .= " LIMIT {$offset}, {$limit}";
+				}
 			}
 
 			$this->tryPdoExcept(function () use (&$ret, $sql, $userId) {
@@ -55,6 +84,8 @@
 						$ret[] = UserAuthHistory::fromArray($row, $this->db, $this->log);
 					}
 				}
+
+				return;
 			}, "Failed to retrieve user histories");
 
 			return $ret;
@@ -70,12 +101,22 @@
 			$ret = 0;
 
 			$this->tryPdoExcept(function () use (&$ret, $userId) {
-				$stmt = $this->db->prepare("SELECT COUNT(*) FROM {$this->uahObj->getDbTableName()} WHERE [UserID] = :userId");
-				$stmt->bindParam(':userId', $userId);
+				$sql = "SELECT COUNT(*) FROM {$this->uahObj->getDbTableName()}";
+
+				if ($this->db->getDriver()->is(PdoDrivers::PDO_SQLSRV)) {
+					$sql .= " WHERE [UserID] = :userId";
+				} else if ($this->db->getDriver()->is(PdoDrivers::PDO_MYSQL)) {
+					$sql .= " WHERE `UserID` = :userId";
+				}
+
+				$stmt = $this->db->prepare($sql);
+				$stmt->bindParam(':userId', $userId, \PDO::PARAM_INT);
 
 				if ($stmt->execute()) {
 					$ret = intval($stmt->fetch()[0]);
 				}
+
+				return;
 			}, "Failed to get user auth history count");
 
 			return $ret;
--- tests/classes/UserAuthHistoryClassTest.php
+++ tests/classes/UserAuthHistoryClassTest.php
@@ -1,4 +1,25 @@
 <?php
 
+	use Zibings\AuthHistoryActions;
+	use Zibings\UserAuthHistories;
 	use Zibings\UserAuthHistory;
+
+	class UserAuthHistoryClassTest extends ZsfTestCase {
+		public function test_Crud() : void {
+			$repo = new UserAuthHistories(self::$db, self::$log);
+
+			$cls           = new UserAuthHistory(self::$db, self::$log);
+			$cls->userId   = 1;
+			$cls->action   = new AuthHistoryActions(AuthHistoryActions::LOGIN);
+			$cls->address  = uniqid();
+			$cls->hostname = uniqid();
+			$cls->notes    = uniqid();
+
+			self::assertTrue($cls->create()->isGood());
+
+			$repo->deleteAllForUser(1);
+
+			return;
+		}
+	}
 
