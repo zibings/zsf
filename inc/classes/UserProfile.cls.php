4d6141c97d1531a7f18456a0795d56aab14b9205
Andrew Male <AndyM84@users.noreply.github.com>
2024-12-01 10:34:55 -0500

    Adding 'l' to accepted display name charset

--- inc/classes/UserProfile.cls.php
+++ inc/classes/UserProfile.cls.php
@@ -133,12 +133,13 @@
 		public static function validDisplayName(string $string) : bool {
 			static $chars = [
 				'a' => true, 'b' => true, 'c' => true, 'd' => true, 'e' => true, 'f' => true, 'g' => true, 'h' => true, 'i' => true,
-				'j' => true, 'k' => true, 'm' => true, 'n' => true, 'o' => true, 'p' => true, 'q' => true, 'r' => true, 's' => true,
-				't' => true, 'u' => true, 'v' => true, 'w' => true, 'x' => true, 'y' => true, 'z' => true, 'A' => true, 'B' => true,
-				'C' => true, 'D' => true, 'E' => true, 'F' => true, 'G' => true, 'H' => true, 'I' => true, 'J' => true, 'K' => true,
-				'L' => true, 'M' => true, 'N' => true, 'O' => true, 'P' => true, 'Q' => true, 'R' => true, 'S' => true, 'T' => true,
-				'U' => true, 'V' => true, 'W' => true, 'X' => true, 'Y' => true, 'Z' => true, '0' => true, '1' => true, '2' => true,
-				'3' => true, '4' => true, '5' => true, '6' => true, '7' => true, '8' => true, '9' => true, '_' => true, '.' => true
+				'j' => true, 'k' => true, 'l' => true, 'm' => true, 'n' => true, 'o' => true, 'p' => true, 'q' => true, 'r' => true,
+				's' => true, 't' => true, 'u' => true, 'v' => true, 'w' => true, 'x' => true, 'y' => true, 'z' => true, 'A' => true,
+				'B' => true, 'C' => true, 'D' => true, 'E' => true, 'F' => true, 'G' => true, 'H' => true, 'I' => true, 'J' => true,
+				'K' => true, 'L' => true, 'M' => true, 'N' => true, 'O' => true, 'P' => true, 'Q' => true, 'R' => true, 'S' => true,
+				'T' => true, 'U' => true, 'V' => true, 'W' => true, 'X' => true, 'Y' => true, 'Z' => true, '0' => true, '1' => true,
+				'2' => true, '3' => true, '4' => true, '5' => true, '6' => true, '7' => true, '8' => true, '9' => true, '_' => true,
+				'.' => true
 			];
 
 			$len = strlen($string);
