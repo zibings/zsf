62ea874bb1eb3b0618fa1edfa6fcd9503b5a78aa
Andrew Male <mamale@outlook.com>
2025-08-20 22:57:31 -0400

    [#47] Fixing issue w/ admin UI builds

--- ui/admin/vitest.config.ts
+++ ui/admin/vitest.config.ts
@@ -1,14 +1,18 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import type { UserConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
-)
+const viteConfig = (await import('./vite.config.js')).default as UserConfig;
+
+export default defineConfig(async () => {
+	return mergeConfig(
+		viteConfig,
+		{
+			test: {
+				environment: 'jsdom',
+				exclude: [...configDefaults.exclude, 'e2e/**'],
+				root: fileURLToPath(new URL('./', import.meta.url))
+			}
+		}
+	)
+});
